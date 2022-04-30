bea5c80849e50bcb3d345db6bef39f858155e028
leeseonjin01 <lsjlsh1130@gmail.com>
2022-04-30 17:50:15 +0900

    graph2

--- myg.py
+++ myg.py
@@ -6,6 +6,7 @@ from lmfit import Model
 import pandas as pd
 from openpyxl import load_workbook
 import statsmodels.api as sm
+from dateutil.parser import parse
 from openpyxl.utils import get_column_letter
 
 xml = etree.parse('HY202103_D08_(0,2)_LION1_DCM_LMZC.xml')
@@ -31,6 +32,14 @@ for i in root.iter('Current'):
     I.extend(list(map(float, i.text.split(','))))
     I = list(map(abs, I))
 
+plt.figure(1, [18, 8])
+plt.subplot(2, 3, 4)
+plt.plot(V, I, 'b.', label='data', markersize=8)
+plt.yscale('log')
+plt.title('I-V analysis', fontsize=16, fontweight='bold')
+plt.xlabel('Voltage[V]', fontsize=13)
+plt.ylabel('Current[A]', fontsize=13)
+
 x = np.array(V[:])
 y = np.array(I[:])
 fit1 = np.polyfit(x, y, 12)
@@ -40,7 +49,17 @@ def IV_fit(X, Is, Vt):
     return (Is * (exp(X/Vt) - 1) + fit1(X))
 
 model = Model(IV_fit)
-result = model.fit(y, X=x, Is=10**-15, Vt=0.026)
+result = model.fit(I, X=V, Is=10**-15, Vt=0.026)
+
+initial_list = []
+for i in V:
+    x_value = IV_fit(i, 10e-16, 0.026)
+    initial_list.append(x_value)
+
+initial = sm.add_constant(np.abs(y))
+result1 = sm.OLS(initial_list, initial).fit()
+
+IVdic = {I: V for V, I in zip(result.best_fit, V)}
 
 # R-squared
 def IVR(y):
@@ -52,17 +71,13 @@ def IVR(y):
 
 # R-squared code
 # r2 = r2_score(I, np.polyval(fit1, V))
-# print(r2)
 
-plt.subplot(2, 3, 4)
-plt.plot(V, I, 'b.', label='data', markersize=8)
 plt.plot(x, result.best_fit, label='best_fit')
 plt.plot(x, result.best_fit, 'r-', label='R-squared ={}'.format(IVR(y)))
-plt.legend(loc = 'best', fontsize=6)
-plt.yscale('log')
-plt.title('I-V analysis', fontsize=12, fontweight='bold')
-plt.xlabel('Voltage[V]', fontsize=8)
-plt.ylabel('Current[A]', fontsize=8)
+# plt.plot(x, result.best_fit, 'r-', label=result1.rsquared)
+plt.legend(loc = 'best', fontsize=10)
+plt.text(1.0, IVdic[1.0], IVdic[1.0])
+plt.text(-1.0, IVdic[-1.0], IVdic[-1.0])
 
 # wavelength-transmission graph
 counts = []
@@ -83,10 +98,10 @@ for i in root.iter('WavelengthSweep'):
 DCBias[-1] = 'Reference'
 for r in range(len(L)):
     plt.plot(L[r], IL[r], label=DCBias[r])
-plt.xlabel('Wavelength[nm]', fontsize=8)
-plt.ylabel('Transmission[dB]', fontsize=8)
-plt.title('Transmission spectra as measured', fontsize=12, fontweight='bold')
-plt.legend(loc='best', ncol=2, fontsize=6)
+plt.xlabel('Wavelength[nm]', fontsize=13)
+plt.ylabel('Transmission[dB]', fontsize=13)
+plt.title('Transmission spectra as measured', fontsize=16, fontweight='bold')
+plt.legend(loc='best', ncol=2, fontsize=10)
 
 # reference fitting
 plt.subplot(2, 3, 2)
@@ -94,10 +109,10 @@ plt.plot(L[-1], IL[-1], 'r', label = 'reference')
 fit_L = np.polyfit(L[-1], IL[-1], 2)
 fit_IL = np.polyval(fit_L, L[-1])
 plt.plot(L[-1], fit_IL, 'c-', label='Fit ref polynomial O3')
-plt.legend(loc='best', fontsize=6)
-plt.xlabel('Wavelength[nm]', fontsize=8)
-plt.ylabel('Transmission[dB]', fontsize=8)
-plt.title('Transmission spectra as measured\n(Fitting)', fontsize=12, fontweight='bold')
+plt.legend(loc='best', fontsize=10)
+plt.xlabel('Wavelength[nm]', fontsize=13)
+plt.ylabel('Transmission[dB]', fontsize=13)
+plt.title('Transmission spectra as measured\n(Fitting)', fontsize=16, fontweight='bold')
 plt.ylim(-60, -5)
 
 # reference - fitting
@@ -109,31 +124,37 @@ for j in range(len(L)-1):
     for k in range(len(IL[j])):
         IL_f.append(IL[j][k]-fit_IL[k])
     plt.plot(L[j], IL_f, label=DCBias[j])
-plt.xlabel('Wavelength[nm]', fontsize=8)
-plt.ylabel('Transmission[dB]', fontsize=8)
-plt.title('Transmission spectra as measured\n(Fitting)', fontsize=12, fontweight='bold')
-plt.legend(loc='best', ncol=2, fontsize=6)
+plt.xlabel('Wavelength[nm]', fontsize=13)
+plt.ylabel('Transmission[dB]', fontsize=13)
+plt.title('Transmission spectra as measured\n(Fitting)', fontsize=18, fontweight='bold')
+plt.legend(loc='best', ncol=2, fontsize=10)
 plt.tight_layout()
 
+# save
+plt.suptitle('Filename')
+fig = plt.gcf()
+fig.set_size_inches((27, 15), forward=False)
 plt.savefig('./graph.png')
 
+# xlsx
 TestSiteInfo=root.find('TestSiteInfo')
-dict={'Lot':[],'Wafer':[],'Mask':[],'TestSite':[],'Name':[],'Date':[],'Operator':[],'DieRow':[],'DieColumn':[],'AnalysisWavelength (nm)':[],'Rsq of Ref. spectrum (6th)':[] ,'Max transmission of Ref. spec. (dB)':[], 'Rsq of IV':[], '1 at -1V[A]':[], '1 at 1V[A]':[]}
-AlignWavelength=next(root.iter('AlignWavelength'))
-Modulator=next(root.iter('Modulator'))
+dict = {'Lot':[],'Wafer':[],'Mask':[],'TestSite':[],'Name':[],'Date':[],'Operator':[],'DieRow':[],'DieColumn':[],'AnalysisWavelength (nm)':[],'Rsq of Ref. spectrum (6th)':[] ,'Max transmission of Ref. spec. (dB)':[], 'Rsq of IV':[], '1 at -1V[A]':[], '1 at 1V[A]':[]}
+AlignWavelength = next(root.iter('AlignWavelength'))
+Modulator = next(root.iter('Modulator'))
 
-# rsqref = poly(L[-1], IL, 6)
 IVdic = {I: V for V, I in zip(result.best_fit, V)}
 
 initial_list = []
 for i in x:
-    x_value = IV_fit(i, 10e-16, 1 / 0.026)
+    x_value = IV_fit(i, 10e-16, 0.026)
     initial_list.append(x_value)
-
 initial = sm.add_constant(np.abs(y))
 result1 = sm.OLS(initial_list, initial).fit()
-
 Rsq = result1.rsquared
+
+times = root.attrib['CreationDate']
+times = parse(times).strftime('%Y%m%d_%H%M%S')
+
 
 for i in range(len(L)-1):
     dict['Lot'].append(TestSiteInfo.attrib['Batch'])
@@ -141,24 +162,24 @@ for i in range(len(L)-1):
     dict['Mask'].append(TestSiteInfo.attrib['Maskset'])
     dict['TestSite'].append(TestSiteInfo.attrib['TestSite'])
     dict['Name'].append(Modulator.attrib['Name'])
-    dict['Date'].append(root.attrib['CreationDate'])
+    dict['Date'].append(times)
     dict['Operator'].append(root.attrib['Operator'])
     dict['DieRow'].append(TestSiteInfo.attrib['DieRow'])
     dict['DieColumn'].append(TestSiteInfo.attrib['DieColumn'])
     dict['AnalysisWavelength (nm)'].append(AlignWavelength.text)
     dict['Rsq of Ref. spectrum (6th)'].append(' ')
-    dict['Max transmission of Ref. spec. (dB)'].append(max(IL[i] - fit_IL))
+    dict['Max transmission of Ref. spec. (dB)'].append(max(IL[i]))
     dict['Rsq of IV'].append(Rsq)
     dict['1 at -1V[A]'].append(IVdic[-1.0])
     dict['1 at 1V[A]'].append(IVdic[1.0])
-frame=pd.DataFrame(dict)
+frame = pd.DataFrame(dict)
 frame.to_excel('Data.xlsx',index=False)
-wb=load_workbook('Data.xlsx')
-ws=wb.active
+wb = load_workbook('Data.xlsx')
+ws = wb.active
 for i in ['E','F','J','K','L']:
     ws.column_dimensions[i].width = 30
 ws.column_dimensions['D'].width = 13
 ws.column_dimensions['H'].width = 13
 wb.save('./Data1.xlsx')
 
-# plt.show()
+plt.show()
