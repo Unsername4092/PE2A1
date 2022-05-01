acf23ac639b3626d9b5c98971c433e7bfb9e3dd4
leeseonjin01 <lsjlsh1130@gmail.com>
2022-05-01 12:50:44 +0900

    graph

--- myg.py
+++ myg.py
@@ -4,6 +4,7 @@ import numpy as np
 from numpy import exp
 from lmfit import Model
 import pandas as pd
+from sklearn.metrics import r2_score
 from openpyxl import load_workbook
 import statsmodels.api as sm
 from dateutil.parser import parse
@@ -12,16 +13,6 @@ from openpyxl.utils import get_column_letter
 xml = etree.parse('HY202103_D08_(0,2)_LION1_DCM_LMZC.xml')
 root = xml.getroot()
 
-def poly(x, y, degree):
-    coeffs = np.polyfit(x, y, degree)
-    # r-squared
-    p = np.poly1d(coeffs)
-    yhat = p(x)
-    ybar = np.sum(y) / len(y)           # or sum(y)/len(y)
-    ssreg = np.sum((yhat - ybar) ** 2)  # or sum([ (yihat - ybar)**2 for yihat in yhat])
-    sstot = np.sum((y - ybar) ** 2)     # or sum([ (yi - ybar)**2 for yi in y])
-    results = ssreg / sstot
-    return results
 
 # I-V graph
 V = []
@@ -36,9 +27,9 @@ plt.figure(1, [18, 8])
 plt.subplot(2, 3, 4)
 plt.plot(V, I, 'b.', label='data', markersize=8)
 plt.yscale('log')
-plt.title('I-V analysis', fontsize=16, fontweight='bold')
-plt.xlabel('Voltage[V]', fontsize=13)
-plt.ylabel('Current[A]', fontsize=13)
+plt.title('I-V analysis', fontsize=12, fontweight='bold')
+plt.xlabel('Voltage[V]', fontsize=10)
+plt.ylabel('Current[A]', fontsize=10)
 
 x = np.array(V[:])
 y = np.array(I[:])
@@ -75,7 +66,7 @@ def IVR(y):
 plt.plot(x, result.best_fit, label='best_fit')
 plt.plot(x, result.best_fit, 'r-', label='R-squared ={}'.format(IVR(y)))
 # plt.plot(x, result.best_fit, 'r-', label=result1.rsquared)
-plt.legend(loc = 'best', fontsize=10)
+plt.legend(loc = 'best', fontsize=8)
 plt.text(1.0, IVdic[1.0], IVdic[1.0])
 plt.text(-1.0, IVdic[-1.0], IVdic[-1.0])
 
@@ -95,24 +86,43 @@ for i in root.iter('WavelengthSweep'):
     site_key = list(i.attrib.keys())
     site_values = list(i.attrib.values())
     DCBias.append(site_key[1]+'='+site_values[1])
+
 DCBias[-1] = 'Reference'
 for r in range(len(L)):
     plt.plot(L[r], IL[r], label=DCBias[r])
-plt.xlabel('Wavelength[nm]', fontsize=13)
-plt.ylabel('Transmission[dB]', fontsize=13)
-plt.title('Transmission spectra as measured', fontsize=16, fontweight='bold')
-plt.legend(loc='best', ncol=2, fontsize=10)
+plt.xlabel('Wavelength[nm]', fontsize=10)
+plt.ylabel('Transmission[dB]', fontsize=10)
+plt.title('Transmission spectra as measured', fontsize=12, fontweight='bold')
+plt.legend(loc='best', ncol=2, fontsize=8)
 
+
+def poly(x, y, deg):
+    coeffs = np.polyfit(x, y, deg)
+    # r-squared
+    p = np.poly1d(coeffs)
+    yhat = p(x)
+    ybar = np.sum(y) / len(y)           # or sum(y)/len(y)
+    ssreg = np.sum((yhat - ybar) ** 2)  # or sum([ (yihat - ybar)**2 for yihat in yhat])
+    sstot = np.sum((y - ybar) ** 2)     # or sum([ (yi - ybar)**2 for yi in y])
+    results = ssreg / sstot
+    return results
+
+Rref=[]
+for i in range(2, 7):
+    Rref.append(poly(L[-1], IL[-1], i))
+print(Rref)
 # reference fitting
 plt.subplot(2, 3, 2)
 plt.plot(L[-1], IL[-1], 'r', label = 'reference')
-fit_L = np.polyfit(L[-1], IL[-1], 2)
-fit_IL = np.polyval(fit_L, L[-1])
-plt.plot(L[-1], fit_IL, 'c-', label='Fit ref polynomial O3')
-plt.legend(loc='best', fontsize=10)
-plt.xlabel('Wavelength[nm]', fontsize=13)
-plt.ylabel('Transmission[dB]', fontsize=13)
-plt.title('Transmission spectra as measured\n(Fitting)', fontsize=16, fontweight='bold')
+for i in range(2, 7):
+    fit_L = np.polyfit(L[-1], IL[-1], i)
+    fit_IL = np.polyval(fit_L, L[-1])
+    plt.plot(L[-1], fit_IL, 'c-', label='{0}th Rsqure={1}'.format(i, Rref[i-2]))
+
+plt.legend(loc='best', fontsize=8)
+plt.xlabel('Wavelength[nm]', fontsize=10)
+plt.ylabel('Transmission[dB]', fontsize=10)
+plt.title('Transmission spectra as measured(Fitting)', fontsize=12, fontweight='bold')
 plt.ylim(-60, -5)
 
 # reference - fitting
@@ -124,16 +134,17 @@ for j in range(len(L)-1):
     for k in range(len(IL[j])):
         IL_f.append(IL[j][k]-fit_IL[k])
     plt.plot(L[j], IL_f, label=DCBias[j])
-plt.xlabel('Wavelength[nm]', fontsize=13)
-plt.ylabel('Transmission[dB]', fontsize=13)
-plt.title('Transmission spectra as measured\n(Fitting)', fontsize=18, fontweight='bold')
-plt.legend(loc='best', ncol=2, fontsize=10)
+
+plt.xlabel('Wavelength[nm]', fontsize=10)
+plt.ylabel('Transmission[dB]', fontsize=10)
+plt.title('Transmission spectra as measured\n(Fitting)', fontsize=12, fontweight='bold')
+plt.legend(loc='best', ncol=2, fontsize=8)
 plt.tight_layout()
 
 # save
-plt.suptitle('Filename')
+plt.suptitle('Filename', fontsize=13, fontweight='bold')
 fig = plt.gcf()
-fig.set_size_inches((27, 15), forward=False)
+fig.set_size_inches((25, 17), forward=False)
 plt.savefig('./graph.png')
 
 # xlsx
@@ -152,6 +163,7 @@ initial = sm.add_constant(np.abs(y))
 result1 = sm.OLS(initial_list, initial).fit()
 Rsq = result1.rsquared
 
+
 times = root.attrib['CreationDate']
 times = parse(times).strftime('%Y%m%d_%H%M%S')
 
@@ -167,19 +179,20 @@ for i in range(len(L)-1):
     dict['DieRow'].append(TestSiteInfo.attrib['DieRow'])
     dict['DieColumn'].append(TestSiteInfo.attrib['DieColumn'])
     dict['AnalysisWavelength (nm)'].append(AlignWavelength.text)
-    dict['Rsq of Ref. spectrum (6th)'].append(' ')
+    dict['Rsq of Ref. spectrum (6th)'].append(Rref[i-1])
     dict['Max transmission of Ref. spec. (dB)'].append(max(IL[i]))
     dict['Rsq of IV'].append(Rsq)
     dict['1 at -1V[A]'].append(IVdic[-1.0])
     dict['1 at 1V[A]'].append(IVdic[1.0])
 frame = pd.DataFrame(dict)
-frame.to_excel('Data.xlsx',index=False)
-wb = load_workbook('Data.xlsx')
-ws = wb.active
-for i in ['E','F','J','K','L']:
-    ws.column_dimensions[i].width = 30
-ws.column_dimensions['D'].width = 13
-ws.column_dimensions['H'].width = 13
-wb.save('./Data1.xlsx')
+print(frame)
+frame.to_csv('Data.csv',index=False)
+# wb = load_workbook('Data.xlsx')
+# ws = wb.active
+# for i in ['E','F','J','K','L']:
+#     ws.column_dimensions[i].width = 30
+# ws.column_dimensions['D'].width = 13
+# ws.column_dimensions['H'].width = 13
+# wb.save('./Data1.xlsx')
 
 plt.show()
